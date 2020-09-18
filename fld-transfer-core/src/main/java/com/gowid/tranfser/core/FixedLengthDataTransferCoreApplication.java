de137673abf945209635e41061af0521060a7550
leo <lhjang@mybroccoli.co.kr>
2020-09-18 17:24:57 +0900

    Initial commit

---
+++ fld-transfer-core/src/main/java/com/gowid/tranfser/core/FixedLengthDataTransferCoreApplication.java
@@ -0,0 +1,11 @@
+package com.gowid.tranfser.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedLengthDataTransferCoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FixedLengthDataTransferCoreApplication.class, args);
+	}
+}
