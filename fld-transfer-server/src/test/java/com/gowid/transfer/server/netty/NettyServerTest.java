0a0ff85dee9131969ae8165f1e9e8999df898dd6
leo <navyguy113@gmail.com>
2021-03-18 18:03:58 +0900

    fix: add method nettyserver

--- fld-transfer-server/src/test/java/com/gowid/transfer/server/netty/NettyServerTest.java
+++ fld-transfer-server/src/test/java/com/gowid/transfer/server/netty/NettyServerTest.java
@@ -1,18 +1,14 @@
 package com.gowid.transfer.server.netty;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class NettyServerTest {
 
-    @Autowired
     private NettyServer nettyServer;
 
     @Test
     void run() throws InterruptedException {
-        nettyServer.run();
+        nettyServer.connect();
         //TODO : not implemented
     }
 
