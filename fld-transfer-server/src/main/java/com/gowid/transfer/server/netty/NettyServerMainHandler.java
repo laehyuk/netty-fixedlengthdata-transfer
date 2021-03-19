1df93dec3151a52b5f3db3a917fd7c9842eadc83
leo <navyguy113@gmail.com>
2021-03-19 23:00:49 +0900

    feat: echo in netty server main handler

--- fld-transfer-server/src/main/java/com/gowid/transfer/server/netty/NettyServerMainHandler.java
+++ fld-transfer-server/src/main/java/com/gowid/transfer/server/netty/NettyServerMainHandler.java
@@ -1,31 +1,33 @@
 package com.gowid.transfer.server.netty;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
 
 @Slf4j
 @ChannelHandler.Sharable
 public class NettyServerMainHandler extends ChannelInboundHandlerAdapter {
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        String message = ((ByteBuf) msg).toString(Charset.defaultCharset());
+        log.info("channel read : {}", message);
+        ctx.write(msg);
     }
 
     @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        super.channelInactive(ctx);
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        super.channelReadComplete(ctx);
+    public void exceptionCaught( ChannelHandlerContext ctx, Throwable cause ) {
+        log.error("An exception has occurred in netty server.");
+        cause.printStackTrace();
+        ctx.close();
     }
 }
