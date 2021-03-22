1816764cca71dd9a841531f2ed4500e2cc5d8e6e
leo <navyguy113@gmail.com>
2021-03-22 23:03:44 +0900

    feat: define fixed data

--- fld-transfer-core/src/main/java/com/gowid/tranfser/core/domain/account/Account.java
+++ fld-transfer-core/src/main/java/com/gowid/tranfser/core/domain/account/Account.java
@@ -5,21 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.saro.commons.bytes.annotations.FixedData;
+import me.saro.commons.bytes.annotations.FixedText;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-@FixedData
+@FixedData(size = 100)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Account {
 
     @Id
+    @FixedText(offset = 0, length = 10)
     private String id;
 
+    @FixedText(offset = 10, length = 20)
     private String owner;
 
+    @FixedText(offset = 30, length = 70)
     private Double value;
 
 }
